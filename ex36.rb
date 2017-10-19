6d1632112e8bb13307831951472cf41e6ce92087
Eric Gray <ericxgray@gmail.com>
2017-10-18 17:58:26 -0700

    More rooms added as placeholders. Need descriptions, and items. All connect correctly.

--- ex36.rb
+++ ex36.rb
@@ -113,7 +113,22 @@ end
 def show_room(roomNumber)
    rooms = [
   [['sword', 'shield'], [1, nil, nil, nil, nil, nil], "You are standing in the entrance of a dark cave.\nThe mouth of this cave is only barely taller than\nyou are and leads into a vast darkness."],
-  [[], [nil, 4, 0, 3, nil, nil], "Here is the next room. It strangely\n looks like an office lobby. A woman\n with a nameplate which reads 'Jessica' \nis looking at you."]
+  [[], [nil, 2, 0, 3, nil, nil], "Here is the next room. It strangely\n looks like an office lobby. A woman\n with a nameplate which reads 'Jessica' \nis looking at you."],
+  [[], [nil, nil, nil, 1, nil, nil], "You are in a security room! It is not parcitularly exciting.\nOn the one of the screens you can see the room\nyou just came from. Jessica sits idly at the desk."],
+  [[], [nil, 1, nil, nil, 4, nil], "This is an elevator. There is a panel with\n buttons which will take you up or down."],
+  [[], [nil, 7, nil, nil, 5, 3], "This is the second floor elevator. You can take\n it up or down, or exit to the east."],
+  [[], [nil, 8, nil, nil, 6, 4], "You are on the third floor. You can take the\nelevator up or down, or exit to the east."],
+  [[], [nil, 9, nil, nil, nil, 5], "You are on the top floor. You can take the\nelevator down, or exit to the east."],
+  [[], [11, 10, nil, 4, nil, nil], "This appears to be a lobby similar to the ground\nfloor. Except that all the walls are a cave and the front\nreception desk is a few barrels with wood over the top\nand the receptionist is a goblin."],
+  [[], [nil, 12, nil, 5, nil, nil], "Whoa, it's a lobby but this time it's in the\nfuture! There's a bunch of holograms and other things I should\nspend more time trying to write better!"],
+  [[], [15, 16, nil, 6, nil, nil], "Penthouse! It's so faaaaancy~~"],
+  [[], [nil, nil, nil, 7, nil, nil], "Treasure room!"],
+  [[], [nil, nil, 7, nil, nil, nil], "Rickety bridge!"],
+  [[], [nil, 13, nil, 8, nil, nil], "Sci-fi Market!"],
+  [[], [nil, 14, nil, 12, nil, nil], "Stasis Room."],
+  [[], [nil, nil, nil, 13, nil, nil], "Stasis Pod."],
+  [[], [nil, nil, 9, nil, nil, nil], "Fancy bedroom!"],
+  [[], [nil, nil, nil, 9, nil, nil], "Office!"]
   ]
   return rooms[roomNumber]
 end
